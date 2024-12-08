f281a201899f534f23e5a78c4d956cbae0a39fb2
Victor Chagas <victorchagaslm@gmail.com>
2024-12-08 17:17:06 -0300

    Tentativa de autenticação de usuário

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout() {
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="imageCapture" />
+        <Stack.Screen name="carer" />
       </Stack>
     </GluestackUIProvider>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -2,19 +2,45 @@ import { Box } from "@/components/ui/box";
 import { Button, ButtonGroup, ButtonText } from "@/components/ui/button";
 import { Center } from "@/components/ui/center";
 import { router } from "expo-router";
+import { Alert } from "react-native";
+
+import credential from "@/data/credential.json";
 
 export default function Index() {
+  const handleLowVisionFlux = async () => {
+    try {
+      const response = await fetch(
+        "http://192.168.1.9:3000/proxy/lowVisionAuth",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ id: credential.id }),
+        }
+      );
+
+      const result = await response.json();
+
+      const parsedData = JSON.parse(result?.data ?? "{}");
+      const id = parsedData?.id;
+
+      console.log(id);
+
+      router.push("./imageCapture");
+    } catch (error) {
+      console.error("Erro ao conectar ao servidor intermediário:", error);
+      Alert.alert("Erro", "Falha ao se comunicar com o servidor intermediário");
+    }
+  };
+
   return (
     <Box className="h-full bg-white dark:bg-slate-900">
       <Center className="h-full">
-        <ButtonGroup>
+        <ButtonGroup space="4xl">
           <Button
-            onPress={() => {
-              router.push("./imageCapture");
-            }}
+            onPress={handleLowVisionFlux}
             className="h-auto py-10 px-8 rounded-2xl"
           >
-            <ButtonText className="text-5xl">Auxíliar locomoção</ButtonText>
+            <ButtonText className="text-5xl">Auxíliar de locomoção</ButtonText>
           </Button>
           <Button
             onPress={() => {
