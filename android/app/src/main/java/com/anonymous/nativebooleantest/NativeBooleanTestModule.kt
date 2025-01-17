8c1c32f987edf022031fb935d76791afae763c11
Víctor Chagas <victorchagaslm@gmail.com>
2025-01-17 08:58:30 -0300

    refactor: Subindo a pasta android

---
+++ android/app/src/main/java/com/anonymous/nativebooleantest/NativeBooleanTestModule.kt
@@ -0,0 +1,20 @@
+package com.anonymous.nativebooleantest
+
+import android.content.Context
+import com.anonymous.seesafespecs.NativeBooleanTestSpec
+import com.facebook.react.bridge.ReactApplicationContext
+
+class NativeBooleanTestModule(reactContext: ReactApplicationContext) : NativeBooleanTestSpec(reactContext) {
+
+  override fun getName() = NAME
+
+  override fun getValue(): Boolean {
+    // Exemplo de lógica para retornar um valor booleano
+    return true  // Aqui você pode substituir a lógica para obter o valor booleano desejado
+  }
+
+  companion object {
+    const val NAME = "NativeBooleanTest"
+  }
+}
+  
